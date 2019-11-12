e9823fff22cce27cb93c491811d36d15a25d0402
Cindy Kei <myk245@nyu.edu>
2019-11-12 03:37:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   new_array = []
 
   while counter < array.length do
-    puts array[counter]**2
+    new_array << array[counter]**2
     counter += 1
   end
   return new_array
